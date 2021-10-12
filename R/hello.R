eaf756542946f7d6839064ed518133f0046f0d82
Ziqiu Zheng <2761088430@qq.com>
2021-10-12 12:10:09 -0400

    initial commit

---
+++ R/hello.R
@@ -0,0 +1,25 @@
+
+#' @title Say hello
+#'
+#' @description This function says hello.
+#' @param world a logical indicating if we are saying hello
+#' to the world. (Default TRUE)
+#' @param verbose should the return be printed? (Default FALSE)
+#' @return nothing
+#' @examples
+#' hello(FALSE)
+#' @export
+hello <- function(world = TRUE, verbose = FALSE) {
+  if (!is.logical(world) && !is.logical(verbose)) {
+    stop("Parameters must be logical.")
+  }
+  if (world) {
+    ret <- "Hello, world!"
+  } else {
+    ret <- "Hello!"
+  }
+  if (verbose) {
+    print(ret)
+  }
+  invisible(ret)
+}
