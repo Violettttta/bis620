165d4e6be86b464ed79d9737864faeeea3a6f17e
Ziqiu Zheng <2761088430@qq.com>
2021-10-28 18:56:25 -0400

    Test functions of adam-utils.

--- R/data.R
+++ R/data.R
@@ -8,3 +8,30 @@
 #' @rdname pm_ae
 #' @references NCT00339183
 NULL
+
+#' @title A Demo Data Set
+#'
+#' @description These data were generated from the NCT00364013
+#' @name DEMO
+#' @docType data
+#' @rdname DEMO
+#' @references NCT00364013
+NULL
+
+#' @title A Respeval Data Set
+#'
+#' @description These data were generated from the NCT00364013
+#' @name RESPEVAL
+#' @docType data
+#' @rdname RESPEVAL
+#' @references NCT00364013
+NULL
+
+#' @title An Ae Data Set
+#'
+#' @description These data were generated from the NCT00364013
+#' @name AE
+#' @docType data
+#' @rdname AE
+#' @references NCT00364013
+NULL
--- data-raw/DATASET.R
+++ data-raw/DATASET.R
@@ -8,5 +8,13 @@ pm_ae <- read_sas("NCT00339183/ae.sas7bdat") %>%
     AESTDYI = sample(AESTDYI),
     AESOC = sample(AESOC)
   )
-
 usethis::use_data(pm_ae, overwrite = TRUE)
+
+DEMO <- read_sas("data-raw/demo.sas7bdat")
+usethis::use_data(DEMO, overwrite = TRUE)
+
+RESPEVAL <- read_sas("data-raw/respeval.sas7bdat")
+usethis::use_data(RESPEVAL, overwrite = TRUE)
+
+AE <- read_sas("data-raw/ae.sas7bdat")
+usethis::use_data(AE, overwrite = TRUE)
